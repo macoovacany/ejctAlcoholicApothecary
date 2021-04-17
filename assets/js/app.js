6c6d94296e39b816dd762c5d200352a75784a31d
macoovacany <macoovacany@hotmail.com>
2021-04-18 00:59:41 +0930

    hooked up the suggest-cocktail button

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,54 +33,48 @@ function getAPIIngredients() {
     }
 }
 
-// https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array
-function getRandom(arr, n) {
-    var result = new Array(n),
-        len = arr.length,
-        taken = new Array(len);
-    if (n > len)
-        throw new RangeError("getRandom: more elements taken than available");
-    while (n--) {
-        var x = Math.floor(Math.random() * len);
-        result[n] = arr[x in taken ? taken[x] : x];
-        taken[x] = --len in taken ? taken[len] : len;
-    }
-    return result;
-}
-
-function getRandomIngedients(count) {
-
-}
-
-
-
 
 // ******************************************
-// update Suggested items 
+// Available Drinks section 
 // ******************************************
-drinks = ["Vodka", "Gin", "Tequila"];
-let suggestedCocktailsURL = `https://www.thecocktaildb.com/api/json/v2/${API_KEY_COCKTAIL_DB}/filter.php?i=${drinks.join(',')}`
-fetch(suggestedCocktailsURL)
-    .then(response => response.json())
-    .then(data => {
-        // build the html list items from the drinks arrat
-        html = '';
-        data.drinks.forEach(drink => {
-            html = html + suggestedCocktailLI(drink);
-        });
-        // update the DOM
-        $("#suggested-cocktails").html(`<ul>${html}</ul>`);
-    })
+
+$('#ingredients-selection-ui').on("click", (e) => {
+    console.log("clicked on an ingredient");
+    // get the target of the click
+
+    // toggle the data attribute of the ingredient
+    // 
+});
+
+
+$('#suggestCocktailsButton').on("click", (e) => {
+    console.log("clicked the suggest a cocktail button");
+
+    drinks = ["Vodka", "Gin", "Tequila"];
+    let suggestedCocktailsURL = `https://www.thecocktaildb.com/api/json/v2/${API_KEY_COCKTAIL_DB}/filter.php?i=${drinks.join(',')}`
+
+    fetch(suggestedCocktailsURL)
+        .then(response => response.json())
+        .then(data => {
+            // build the html list items from the drinks arrat
+            html = '';
+            data.drinks.forEach(drink => {
+                html = html + suggestedCocktailLI(drink);
+            });
+            // update the DOM
+            $("#suggested-cocktails").html(`<ul>${html}</ul>`);
+        })
+});
 
 
 function suggestedCocktailLI(cocktail) {
     html = `<li>
-    <div class="w-full max-w-sm overflow-hidden rounded border bg-white shadow">
-    <div class="relative">
-    <div class="h-48 bg-cover bg-no-repeat bg-center" style="background-image: url(${cocktail.strDrinkThumb}"></div>
-    <div style="background-color: rgba(0,0,0,0.6)"></div>
-    <div style="bottom: -20px;" class="absolute right-0 w-10 mr-2">
-            <img class="rounded-full border-2 border-white">
+        <div class="w-full max-w-sm overflow-hidden rounded border bg-white shadow">
+        <div class="relative">
+        <div class="h-48 bg-cover bg-no-repeat bg-center" style="background-image: url(${cocktail.strDrinkThumb}"></div>
+        <div style="background-color: rgba(0,0,0,0.6)"></div>
+        <div style="bottom: -20px;" class="absolute right-0 w-10 mr-2">
+        <img class="rounded-full border-2 border-white">
             </div>
             <div class="p-3" data-cocktail-id="${cocktail.idDrink}">
             <h3 class="mr-10 text-sm truncate-2nd">
@@ -124,6 +118,8 @@ const $modalOK = document.querySelector("#modalOK");
 const $modalList = document.querySelector("#modalList");
 const $modalItem = document.querySelector("#modalItem");
 const $modalSpan = document.querySelector("#modalSpan");
+
+
 $addStockButton.addEventListener("click", () => {
     $addStockModal.removeClass("hidden");
     $addStockModal.addClass("add-stock-modal");
@@ -146,24 +142,24 @@ $addStockButton.addEventListener("click", () => {
 });
 
 
-$modalItem.addEventListener("click", (e) => {
-    if (e.currentTarget.dataset.toggle === "true") {
-        e.currentTarget.removeClass("selected");
-        e.currentTarget.querySelector("#modalSpan").removeClass("spanSelected");
-        e.currentTarget.dataset.toggle = "false";
-    } else {
-        e.currentTarget.addClass("selected");
-        e.currentTarget.querySelector("#modalSpan").addClass("spanSelected");
-        e.currentTarget.dataset.toggle = "true";
-    };
-});
+// $modalItem.addEventListener("click", (e) => {
+//     if (e.currentTarget.dataset.toggle === "true") {
+//         e.currentTarget.removeClass("selected");
+//         e.currentTarget.querySelector("#modalSpan").removeClass("spanSelected");
+//         e.currentTarget.dataset.toggle = "false";
+//     } else {
+//         e.currentTarget.addClass("selected");
+//         e.currentTarget.querySelector("#modalSpan").addClass("spanSelected");
+//         e.currentTarget.dataset.toggle = "true";
+//     };
+// });
 
-$modalCancel.addEventListener("click", () => {
-    // TODO
-});
+// $modalCancel.addEventListener("click", () => {
+//     // TODO
+// });
 
-$modalOK.addEventListener("click", () => {
-    // TODO
-});
+// $modalOK.addEventListener("click", () => {
+//     // TODO
+// });
 
-// end of file
+// // end of file
