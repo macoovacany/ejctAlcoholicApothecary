c3ad590602c24921cc9d23c2c53e22c37dc3b446
Emma Mattner <emattner1997@gmail.com>
2021-04-19 21:49:23 +0930

    updating styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -101,14 +101,14 @@ $('#suggestCocktailsButton').on("click", (e) => {
 
 function suggestedCocktailTemplate(cocktail) {
     html = `<li>
-        <div class="w-full max-w-sm overflow-hidden rounded border bg-white shadow">
+        <div class="w-full overflow-hidden rounded border bg-white shadow text-center">
         <div class="relative">
-        <div class="h-48 bg-cover bg-no-repeat bg-center" style="background-image: url(${cocktail.strDrinkThumb}"></div>
+        <div class="suggested-cocktail-height bg-cover bg-no-repeat bg-center" style="background-image: url(${cocktail.strDrinkThumb}"></div>
         <div style="background-color: rgba(0,0,0,0.6)"></div>
-        <div style="bottom: -20px;" class="absolute right-0 w-10 mr-2">
+        <div style="bottom: -20px;" class="right-0 w-10 mr-2">
         <img class="rounded-full border-2 border-white">
             </div>
-            <div class="p-3" data-cocktail-id="${cocktail.idDrink}">
+            <div class="p-4" data-cocktail-id="${cocktail.idDrink}">
             <h3 class="mr-10 text-sm truncate-2nd">
                 <a class="hover:text-blue-500" href="#">${cocktail.strDrink}</a>
             </h3>
