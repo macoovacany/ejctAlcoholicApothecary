2ab468cb6b1a6b70a413d79b371e47e2d848ae7f
Jared Rice <ricjm008@mymail.unisa.edu.au>
2021-04-19 18:50:39 +0930

    w3 prototype

--- assets/js/app.js
+++ assets/js/app.js
@@ -162,7 +162,7 @@ $addStockButton.addEventListener("click", () => {
     $addStockModal.addClass("add-stock-modal");
     $modalList.empty();
     document.body.append(`
-    <div class="close"></div>
+    <div class="modal-bg"></div>
     `);
     const allIngredients = JSON.parse(localStorage.getItem("api-ingredients"));
     allIngredients.forEach((i) => {
@@ -177,7 +177,32 @@ $addStockButton.addEventListener("click", () => {
         `)
     });
 });
+// w3 insert 181-205
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 //  ***************************
 // modalItem is missing
 
